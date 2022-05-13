081d6cade6da323f46dc54ad0a31fa418d4d2fb7
Pratiksha Bramhane <pbramhane43@gmail.com>
2022-05-13 22:09:29 +0530

    Login and Registration

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,9 +6,9 @@ from .models import Account
 
 
 class AccountAdmin(UserAdmin):
-    list_display = ['email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined','is_active', ]
-    list_display_links = ['email', 'first_name', 'last_name']
-    readonly_fields = ('last_login', 'date_joined')
+    list_display = ['email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active', ]
+    list_display_links = ['email', 'first_name', 'last_name',]
+    readonly_fields = ('last_login', 'date_joined',)
     ordering = ('-date_joined',)
 
     filter_horizontal = ()
