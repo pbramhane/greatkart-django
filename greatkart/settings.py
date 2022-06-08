db3e4e424136cf280c6bcdd7861c2e7f5cda74ab
Pratiksha Bramhane <pbramhane43@gmail.com>
2022-06-08 15:51:55 +0530

    product orders and payments

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'store',
     'carts',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -146,5 +147,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'pratikshabramhane.pb@gmail.com'
-EMAIL_HOST_PASSWORD = 'Gaurav@18'
+EMAIL_HOST_PASSWORD = 'cuanvdtwdjflxqam'
 EMAIL_USE_TLS = True
